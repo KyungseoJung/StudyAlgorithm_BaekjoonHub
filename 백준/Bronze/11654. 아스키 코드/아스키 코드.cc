e0a23ca61aaab8c1a284227de5b96cfd6e27f2e0
KyungseoJung <123443645+KyungseoJung@users.noreply.github.com>
2025-05-31 23:13:35 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	std::string input;
+
+	std::cin >> input;
+
+	//std::cout << input[0] << " " << input[1] << "\m";
+	std::cout << (int)input[0];
+
+	return 0;
+}
+
+
+	//std::cout << S[num - 1];
+	//std::cout << S.at(num-1);
+	//https://m.blog.naver.com/dorergiverny/223046924132
