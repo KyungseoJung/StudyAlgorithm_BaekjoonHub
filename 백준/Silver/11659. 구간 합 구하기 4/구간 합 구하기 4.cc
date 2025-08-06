06a6aec76610335352e7dfaaa19c3f5808c457bc
KyungseoJung <123443645+KyungseoJung@users.noreply.github.com>
2025-08-06 14:56:09 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 3196 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int N, M;	// 수의 개수 N. 합을 구해야 하는 횟수 M
+	cin >> N >> M;
+
+	vector <int> nums(N);
+	vector <long long int> sums(N);	// 미리 누적합을 계산해놓기 (예: sums[1] = 0~1번째 수까지 더한 값)
+	//int input;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> nums[i];
+		if (i == 0)
+			sums[i] = nums[i];
+		if (i >= 1)
+		{
+			sums[i] = sums[i - 1] + nums[i];
+		}
+	}
+
+	int start, end;
+	int sum;
+	while (M--)
+	{
+		sum = 0;
+		cin >> start >> end;
+		
+		if (start - 2 < 0)
+		{
+			//cout << "start-2 = " << start - 2 << "\n";
+			sum = sums[end - 1];
+
+		}
+		else
+		{
+			sum = sums[end - 1] - sums[start - 2];
+
+		}
+		cout << sum << "\n";
+	}
+
+	return 0;
+}
