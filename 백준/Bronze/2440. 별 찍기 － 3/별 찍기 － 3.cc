72d2d299093fa236d07dbe0060ac6c55a63a8e7e
KyungseoJung <123443645+KyungseoJung@users.noreply.github.com>
2025-10-09 14:31:52 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <string>	// stoi 함수
+
+using namespace std;
+int main()
+{
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < (N - i); j++)
+		{
+			cout << "*";
+
+		}
+		cout << "\n";
+	}
+    return 0;
+}
