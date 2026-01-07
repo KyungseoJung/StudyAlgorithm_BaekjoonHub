7a0dc49bc8f415a931600ebdd78dd8bcdbc845cb
KyungseoJung <123443645+KyungseoJung@users.noreply.github.com>
2026-01-07 22:22:00 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+    
+    long long int a,b;
+    while(N--)
+    {
+        cin >>  a>> b;
+        cout << (a+b) << "\n";
+    }
+    return 0;
+}
