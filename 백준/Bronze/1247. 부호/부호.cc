aae987e00487341cb2c14b2c5c8c5036bad39146
KyungseoJung <123443645+KyungseoJung@users.noreply.github.com>
2026-02-03 16:27:14 +0900

    [Bronze III] Title: 부호, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1247. 부호/부호.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+// 1247번 문제: 부호
+
+/*
+long long int와 double 비교:
+(long double은 사실상 double과 거의 유사하대)
+
+1. 정확한 정수 값이 중요하다면 무조건 long long을 쓰세요.
+2. 소수점이 필요하고 정밀도가 낮아도 되면 float, 높아야 하면 double을 씁니다.
+3. 입력하신 19자리 숫자는 double의 정밀도(15자리)를 초과하기 때문에 값이 왜곡됩니다.
+S
+*/
+int main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+	int Num = 3;
+	
+	int N;
+
+	long long int input;
+	__int128_t sum;
+	while (Num--)
+	{
+		sum = 0;
+		cin >> N;
+		for (int i = 0; i < N; i++)
+		{
+			cin >> input;
+			sum += input;
+		}
+
+		if (sum > 0)
+		{
+			cout << "+\n";
+		}
+		else if (sum < 0)
+		{
+			cout << "-\n";
+		}
+		else
+		{
+			cout << "0\n";
+		}
+	}
+	return 0;
+}
