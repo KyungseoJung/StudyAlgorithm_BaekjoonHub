de04b43c1c3aa978c27ec065437bfabe44d8f475
KyungseoJung <123443645+KyungseoJung@users.noreply.github.com>
2026-04-04 23:38:13 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -1,6 +1,11 @@
 #include <iostream>
 
 using namespace std;
+// 2744번 문제: 대소문자 바꾸기
+
+// toupper와 tolower는 기본적으로 int를 출력한대.
+// 그래서, char = tolower('A') <- 이런식으로 받으면, 자동으로 변환이 되지만
+// 출력할 때에는 숫자가 출력되기 때문에 (char) 변수형 변환이 필요하대
 int main()
 {
 	string input;
