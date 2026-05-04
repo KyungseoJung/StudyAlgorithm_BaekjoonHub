82372ff52242e8c2c30e7109e6082549c178daf6
KyungseoJung <123443645+KyungseoJung@users.noreply.github.com>
2026-05-04 21:11:54 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    for(int i=1; i<=1000; i++)
+    {
+        if(i * i == n)
+        {
+            answer = 1;
+            break;
+        }
+        else if(i * i > n)
+        {
+            answer = 2;
+            break;
+        }
+    }
+    return answer;
+}
