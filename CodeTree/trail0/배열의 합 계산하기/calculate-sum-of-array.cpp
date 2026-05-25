ba375cfe1ff1ba818757b9490d7f0211524967b4
KyungseoJung <123443645+KyungseoJung@users.noreply.github.com>
2026-05-25 22:01:10 +0900

    [Passed] 배열의 합 계산하기 (2ms, 0MB)

---
+++ CodeTree/trail0/배열의 합 계산하기/calculate-sum-of-array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[5] = {3, 1, 4, 5, 9};
+    int sum = arr[1] + arr[3] + arr[4];
+    cout << sum;
+    return 0;
+}
