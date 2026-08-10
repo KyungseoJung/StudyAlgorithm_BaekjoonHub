b96ea275f0aee1ada5b4463580fc86414b0414db
KyungseoJung <123443645+KyungseoJung@users.noreply.github.com>
2026-08-10 16:30:57 +0900

    [level 0] Title: 소인수분해, Time: 0.02 ms, Memory: 4.89 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    vector <int> nums;
+    while(n > 1)
+    {
+        for(int i=2; i<=n; i++)
+        {
+            if(n % i == 0)
+            {
+                nums.push_back(i);
+                n = n / i;
+                break;
+            }
+        }
+    }
+    
+    // nums 중에서 겹치는 숫자 빼고, 고유한 숫자만 answer에 넣기
+    // 어차피 오름차순으로 들어가있을테니까
+    int ele = 0;
+    for(int i=0; i<nums.size(); i++)
+    {
+        if(nums[i] != ele)
+        {
+            answer.push_back(nums[i]);
+            ele = nums[i];
+        }
+    }
+    return answer;
+}
