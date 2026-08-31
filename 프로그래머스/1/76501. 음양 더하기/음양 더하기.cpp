e3b3ebb4e0b4c5d9717916427625d3539551102f
KyungseoJung <123443645+KyungseoJung@users.noreply.github.com>
2026-08-31 12:51:29 +0900

    [level 1] Title: 음양 더하기, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    int n = absolutes.size();
+    for(int i=0; i<n; i++)
+    {
+        if(signs[i] == true)
+        {
+            answer += absolutes[i];
+        }
+        else
+        {
+            answer -= absolutes[i];
+        }
+    }
+    return answer;
+}
