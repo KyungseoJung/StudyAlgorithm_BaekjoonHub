6eca34e2231ed72bad30238831fab4c68b5bda61
KyungseoJung <123443645+KyungseoJung@users.noreply.github.com>
2026-09-10 15:43:08 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>    // sort 함수 사용 목적
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    sort(d.begin(), d.end());
+    
+    int sum = 0;
+    for(int i=0; i<d.size(); i++)
+    {
+        sum += d[i];
+        if(sum <= budget)
+        {
+            answer ++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return answer;
+}
