1f46e1403aba86a20fd867f3cd2d80e8ef28980c
KyungseoJung <123443645+KyungseoJung@users.noreply.github.com>
2026-09-11 14:50:55 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 4.67 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> number) {
+    int answer = 0;
+    int sum;
+    for(int i=0; i<number.size(); i++)
+    {
+        for(int j=i+1; j<number.size(); j++)
+        {
+            for(int k = j+1; k<number.size(); k++)
+            {
+                if((i!=j) && (i!=k) && (j!=k))
+                {
+                    sum = number[i] + number[j] + number[k];
+                    if(sum == 0)
+                    {
+                        answer++;
+                    }
+                }
+            }
+        }
+    }
+    return answer;
+}
